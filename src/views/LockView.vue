7348e1fda15b204588d0f0877c668a8d80a07b34
Zack Wins <el_hefe@mbp.attlocal.net>
2022-03-19 23:05:05 -0400

    updated UI

--- src/views/LockView.vue
+++ src/views/LockView.vue
@@ -4,7 +4,7 @@
   >
     <div class="grid grid-cols-3">
       <div class="col-span-1 px-4">
-        <div class="bg-gray-900 rounded-xl h-96">
+        <div class="bg-gray-900 rounded-xl h-screen overflow-y-scroll">
           <div
             class="p-2 m-2 flex text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-blue-500 via-indigo-600 to-purple-500"
           >
@@ -22,7 +22,18 @@
             class="p-2 m-0.5 bg-black rounded-xl"
             @click="loadLock(jig)"
           >
-            {{ jig.message }}
+            <div v-if="jig.message?.includes(':')">
+              <div class="text-left text-xs text-gray-200 flex">
+                <div class="w-full">{{ jig.message.split(":")[0] }}</div>
+                <div class="text-right text-xs">
+                  {{ jig.message.split(":")[2] }}
+                </div>
+              </div>
+              <div class="text-left">{{ jig.message.split(":")[1] }}</div>
+            </div>
+            <div v-else>
+              <div>{{ jig.message }}</div>
+            </div>
           </div>
           <div v-if="!jigs || jigs.length === 0">
             <div
@@ -34,23 +45,34 @@
         </div>
       </div>
       <div class="col-span-2">
-        <div>
-          {{ selectedLock }}
+        <div v-if="selectedLock" class="bg-gray-900 rounded-xl p-2 m-2">
+          <div v-if="selectedLock.message?.includes(':')">
+            <div class="text-left text-xs text-gray-200 flex">
+              <div class="w-full">{{ selectedLock.message.split(":")[0] }}</div>
+              <div class="text-right text-xs">
+                {{ selectedLock.message.split(":")[2] }}
+              </div>
+            </div>
+            <div class="text-left">
+              {{ selectedLock.message.split(":")[1] }}
+            </div>
+          </div>
+          <div v-else>
+            <div>{{ selectedLock.message }}</div>
+          </div>
         </div>
-        <div class="p-4 m-4">
+        <div class="px-4 m-2">
           <div
             class="p-2 text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-purple-300 via-purple-600 to-pink-500"
           >
-            Create A Lock
+            LockIt
           </div>
           <div class="w-full">
-            <div class="text-lg font-bold w-full pl-4 text-gray-900">
-              Your Name:
-            </div>
-            <div class="">
+            <div class="text-lg font-bold w-full pl-4">Your Name:</div>
+            <div class="text-gray-900">
               <input
                 v-model="name"
-                class="ring-1 ring-indigo-500 rounded-xl bg-gray-100 w-3/4"
+                class="ring-1 ring-indigo-500 rounded-xl bg-gray-100 w-3/4 pl-3"
               />
             </div>
             <div class="text-lg font-bold">Your Message:</div>
@@ -63,16 +85,16 @@
               />
             </div>
             <div>
-              <button @click="createAJig">Create A Jig</button>
+              <button
+                @click="createAJig"
+                class="hover:bg-green-500 ring-1 ring-green-500 rounded-xl p-0.5 m-2 text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-green-300 via-green-600 to-green-700"
+              >
+                SAVE
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- NAV BAR -->
-    <!-- <div>Owner: {{ ownerAddress }} Purse: {{ purseAddress }}</div> -->
-    <div class="">
-      <div>Use My Address:</div>
     </div>
   </div>
 </template>
@@ -98,6 +120,11 @@ export default {
   },
   methods: {
     async createAJig() {
+      if (this.message.length > 240) {
+        alert(
+          "Your message is too long, please upgrade to our pro-subscription"
+        );
+      }
       let _run = new window.Run({
         trust: "*",
         purse: "KwZw2apQ2HJnTbnN6f26YzmYhU9KEmiCrb7HaVDi2MFnXLq6aH38",
@@ -112,7 +139,9 @@ export default {
       // let locked = new LockerClass("lock me ");
       let tx = new window.Run.Transaction();
       tx.update(() => {
-        new LockerClass("Lock Me");
+        new LockerClass(
+          this.name + ":" + this.message + ":" + new Date().toUTCString()
+        );
       });
       let txid = await tx.publish();
       console.log("published", txid);
